e7cd14af0e9d641d228472f494d91e55a355aaa9
leeyunkyung <rachei11539@naver.com>
2017-09-29 23:31:11 +0900

    update villageEnterprise

--- seouri/routes/villageEnterprise.js
+++ seouri/routes/villageEnterprise.js
@@ -19,16 +19,18 @@ const upload = multer({
 });
 
 //카테고리별 마을기업 조회
-// req(param) -> (category(놀거리/ 먹거리/ 구경거리/ 체험거리))
-router.get('/:villageEnterpriseId', async(req, res)=>{
+// req(param) -> (category(놀거리/ 먹거리/ 구경거리/ 체험거리) & location(구정보))
+router.get('/category/:category/:location', async(req, res)=>{
   try{
-    if(!req.params.villageEnterpriseId){
-      res.status(403).send({message: 'please input villageEnterpriseId.'});
+      var category = req.params.category;
+      var location = req.params.location;
+    if(!(category && location)){
+      res.status(403).send({message: 'please input category or location.'});
     }
     else{
       var connection = await pool.getConnection();
-      let query1 = 'select category from villageEnterprise where villageEnterpriseId=?;';
-      let posts = await connection.query(query1, req.params.villageEnterpriseId);
+      let query1 = 'select * from villageEnterprise where category=? and location=?;';
+      let posts = await connection.query(query1, [category, location]);
       res.status(200).send({
         "message":"Succeed in selecting a posts1" ,
         "result" : posts
@@ -41,18 +43,43 @@ router.get('/:villageEnterpriseId', async(req, res)=>{
   }
 });
 
-//특정 마을기업 조회 & 검색시 한번에 연결되는 팝업
+//특정 마을기업 조회
 //req(param) -> (villageEnterpriseId)
 router.get('/detail/:villageEnterpriseId', async(req, res)=>{
   try{
-    if(!req.params.villageEnterpriseId){
+    var id = req.params.villageEnterpriseId;
+    if(!id){
       res.status(403).send({message: 'please input villageEnterpriseId.'});
     } else{
       var connection = await pool.getConnection();
       let query1 = 'select * from villageEnterprise where villageEnterpriseId=?;';
-      let posts = await connection.query(query1, req.params.villageEnterpriseId);
+      let posts = await connection.query(query1, id);
       res.status(200).send({
         "message":"Succeed in selecting a posts2" ,
+        "result" : posts
+      });
+    }
+  }catch(err){
+      res.status(500).send({"message" : "server err"});
+  } finally{
+    pool.releaseConnection(connection);
+  }
+});
+
+//검색시 한번에 연결되는 팝업
+//req(param) -> (name)
+router.get('/:name', async(req, res)=>{
+  try{
+    var name = req.params.name;
+    if(!name){
+      res.status(403).send({message: 'please input name.'});
+    } else{
+      var connection = await pool.getConnection();
+      let query1 = 'select * from villageEnterprise where name=?;';
+      let posts = await connection.query(query1, name);
+
+      res.status(200).send({
+        "message":"Succeed in selecting a posts3" ,
         "result" : posts
       });
     }
