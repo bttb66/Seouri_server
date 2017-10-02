89e8454bacdc50dc9e2c2277a623375fc9d72471
ankim2son@gmail.com <ankim2son@gmail.com>
2017-10-02 21:14:03 +0900

    Update app.js

--- seouri/app.js
+++ seouri/app.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var post = require('./routes/post');
-
+var home = require('./routes/home');
 var app = express();
 
 // view engine setup
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/post', post);
-
+app.use('/home',home);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
