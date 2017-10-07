f4390a9e362bc6fe3c7c0b2eb1a5eaaa5f7a6507
BTTB66\박세원 <backtothebasic6@gmail.com>
2017-10-07 17:41:43 +0900

    update comm profile, create mypage

--- seouri/routes/community.js
+++ seouri/routes/community.js
@@ -2,9 +2,7 @@ const express = require('express');
 const aws = require('aws-sdk');
 const router = express.Router();
 const pool = require('../config/db_pool.js');
-aws.config.loadFromPath('./config/aws_config.json');
 const s3 = new aws.S3();
-const moment = require('moment');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const upload = multer({
@@ -18,161 +16,79 @@ const upload = multer({
   })
 });
 
-
-//게시글 작성
-//req -> (userId, title, content, images(최대5개 배열로), location)
-router.post('/', upload.array('images', 5), async(req, res)=>{
+//회원가입 (카카오톡 토큰(id 로 사용), 회원정보 받기) (name, userId, profile)
+router.post('/', async(req, res) => {
   try{
-     if(!(req.body.title&&req.body.content&&req.body.userId&&req.body.location)){
-       res.status(403).send({ message: 'please input all of title, content, userId.'});
-     } else{
-       var connection = await pool.getConnection();
-       var userId = req.body.userId;
-       let query = 'select name from user where userId=?'
-       var name = await connection.query(query, userId);
+    if(!(req.body.name && req.body.userId)){
+      res.status(403).send({ message: 'please input all of name, userId.'});
+    }else{
 
-       //post 테이블에 게시글 데이터 삽입
-       let query2='insert into post set ?';
-       let record = {
-         "userId" : userId,
-         "title" : req.body.title,
-         "content" : req.body.content,
-         "location" : req.body.location,
-         "date" : moment().format('YY.MM.DD HH:mm'),
-         "name" : name[0].name
-       };
-
-       var postId = await connection.query(query2, record);
-       if(req.files){
-         console.log("images");
-         let query3="insert into image (image, postId) values ?";
-         let record3 = [];
-         for(var key in req.files){
-           if(req.files[key]){
-            record2.push([req.files[key].location, postId.insertId]);
-          }
-         }
-
-         console.log(record3);
-         await connection.query(query3, [record3]);
-       }
-
-       res.status(200).send({ "message" : "Succeed in inserting post." });
-     }
-
-  } catch(err){
-    console.log(err);
-    res.status(500).send({
-      "message": "syntax error"
-    });
-  } finally{
-    pool.releaseConnection(connection);
-  }
-});
-
-// 게시글 조회
-// req(param) -> (location(서울시, 구별)...인코딩...)
-router.get('/list/:location', async(req, res) =>{
-  try{
-    if(!req.params.location){
-      res.status(403).send({"message": "please input location."});
-    } else{
       var connection = await pool.getConnection();
-      let query1 = 'select * from post where location=? order by postId desc';
-      let posts = await connection.query(query1, req.params.location);
+      let query = 'insert into user set ?';
+      let record = {
+        "userId" : req.body.userId,
+        "name" : req.body.name,
+        "profile" : req.body.profile
+      };
+
+      await connection.query(query, record);
       res.status(200).send({
-        "message" : "Succeed in selecting posts" ,
-        "result" : posts
-      });
-    }
-  } catch (err){
-    console.log(err);
-    res.status(500).send({
-      "message": "syntax error"
-    });
-  } finally{
-    pool.releaseConnection(connection);
-  }
-});
-
-//특정 게시글 조회
-//req(param) -> (postId)
-router.get('/:postId', async(req, res)=>{
-  try{
-    if(!req.params.postId){
-      res.status(403).send({ "message" : "please input postId." });
-    } else{
-      var connection = await pool.getConnection();
-      let postId = req.params.postId;
-
-      //조회수 +1
-      let query1 = 'update post set view_num = view_num + 1 where postId=?';
-      await connection.query(query1, postId);
-
-      //특정 게시글 정보 가져오기
-      let query2 = 'select * from post where postId=?';
-      let post = await connection.query(query2, postId);
-
-      //특정 게시글 이미지 가져오기
-      let query3 = 'select image from image where postId=?';
-      let images = await connection.query(query3, postId);
-      if(images.length){ //게시글에 이미지 존재할 경우
-        post[0].images = [];
-        for(var key in images) post[0].images.push(images[key].image);
-      }
-
-      //댓글 정보 가져오기
-      let query4 = 'select * from comment where postId=?';
-      let comments = await connection.query(query4, postId);
-
-      res.status(200).send({
-        "message" : "Succeed in selecting post and comments.",
-        "post" : post[0],
-        "images" : images,
-        "comments" : comments
+        "message" : "Succeed in inserting memberInfo."
       });
     }
   }catch(err){
-    console.log(err);
-    res.status(500).send({
-      "message": "syntax error"
-    });
-  } finally{
+    console.log("server err : " + err);
+    res.status(500).send({ "message" : "syntax error" });
+  }finally{
     pool.releaseConnection(connection);
   }
 });
 
-//댓글 작성
-//req -> (userId, content, postId)
-router.post('/comment', async(req, res)=>{
+//token validation test
+router.post('/login', async(req, res) =>{
   try{
-    console.log(req.body.postId);
-    if(!(req.body.postId && req.body.userId && req.body.content)){
-      res.status(403).send({"message": "please input all of postId, userId, content."});
+    if(!req.body.userId){
+      res.status(403).send({ message: 'please input userId(token).'});
+    }else{
+      var connection = await pool.getConnection();
+      let query = 'select * from user where userId=?';
+      var userInfo = await connection.query(query, req.body.userId);
+      if(!userInfo.length){
+        res.status(401).send({ "message" : "userId(token) authorization err"});
+      } else{
+        res.status(200).send({
+          "message" : "Succeed into userId authorization",
+          "userInfo" : userInfo[0]
+        });
+      }
+    }
+  }catch(err){
+    console.log("server err : " + err);
+    res.status(500).send({ "message" : "syntax error" });
+  }finally{
+    pool.releaseConnection(connection);
+  }
+});
+
+router.post('/mypage', async(req, res) =>{
+  try{
+    if(!req.body.userId){
+      res.status(403).send({ message: 'please input userId(token).'});
     } else{
       var connection = await pool.getConnection();
-      var userId = req.body.userId;
-      let query1 = 'select name from user where userId=?';
-      var name = await connection.query(query1, userId);
 
-      let query2 = 'insert into comment set ?';
-      let record2 = {
-        "postId" : req.body.postId,
-        "userId" : req.body.userId,
-        "content" : req.body.content,
-        "date" : moment().format('YY.MM.DD HH:mm'),
-        "name" : name[0].name
-      };
-      await connection.query(query2, record2);
-      res.status(200).send({"message": "Succeed in inserting comment."});
+      let query = 'select * from post where userId=?';
+      var myPosts = await connection.query(query, req.body.userId);
+      res.status(200).send({
+        "message" : "Succeed into select info",
+        "myPosts" : myPosts
+     });
     }
   } catch(err){
-    console.log(err);
-    res.status(500).send({
-      "message": "syntax error"
-    });
+    console.log("server err : " + err);
+    res.status(500).send({ "message" : "syntax error" });
   } finally{
-    pool.releaseConnection(connection);
+    pool.releaseConnection();
   }
 });
 
